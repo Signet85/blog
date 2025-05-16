861aab29bcbdb9b3209f1e25a67ea2d1f79c9669
Tenges <152248859+Signet85@users.noreply.github.com>
2025-05-16 08:48:56 +0800

    12345

--- app/page.js
+++ app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>Hello world 1234</div>;
+  return <div className={styles.page}>Hello world 12345</div>;
 }
